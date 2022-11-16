952725b97330a9b4c6cb9884e90bfe104cf71334
AzzarRizky <azzar.arza18@gmail.com>
2022-11-16 14:56:51 +0700

    add glide

--- app/src/main/java/com/azzar/pahlawanku/dataPahlawan.java
+++ app/src/main/java/com/azzar/pahlawanku/dataPahlawan.java
@@ -1,7 +1,9 @@
 package com.azzar.pahlawanku;
 
+import java.util.ArrayList;
+
 public class dataPahlawan {
-    public static  String [][] data = new String[][]{
+    public static String[][] data = new String[][]{
             {"Ahmad Dahlan", "Salah seorang ulama dan khatib terkemuka di Masjid Besar Kasultanan Yogyakarta pada masa itu, dan ibu dari K.H. Ahmad Dahlan adalah puteri dari H. Ibrahim yang juga menjabat penghulu Kesultanan Ngayogyakarta Hadiningrat pada masa itu. KH. Ahmad Dahlan telah mempelopori kebangkitan ummat Islam untuk menyadari nasibnya sebagai bangsa terjajah yang masih harus belajar dan berbuat.", "https://upload.wikimedia.org/wikipedia/commons/8/87/Ahmad_Dahlan.jpg"},
             {"Ahmad Yani", "Jenderal TNI Anumerta Ahmad Yani (juga dieja Achmad Yani; lahir di Purworejo, Jawa Tengah, 19 Juni 1922 – meninggal di Lubang Buaya, Jakarta, 1 Oktober 1965 pada umur 43 tahun) adalah komandan Tentara Nasional Indonesia Angkatan Darat, dan dibunuh oleh anggota Gerakan 30 September saat mencoba untuk menculik dia dari rumahnya.", "https://upload.wikimedia.org/wikipedia/commons/3/3f/Ahmad_Yani.jpg"},
             {"Sutomo", "Sutomo (lahir di Surabaya, Jawa Timur, 3 Oktober 1920 – meninggal di Padang Arafah, Arab Saudi, 7 Oktober 1981 pada umur 61 tahun) lebih dikenal dengan sapaan akrab oleh rakyat sebagai Bung Tomo, adalah pahlawan yang terkenal karena peranannya dalam membangkitkan semangat rakyat untuk melawan kembalinya penjajah Belanda melalui tentara NICA, yang berakhir dengan pertempuran 10 November 1945 yang hingga kini diperingati sebagai Hari Pahlawan.", "https://upload.wikimedia.org/wikipedia/commons/e/ed/Bung_Tomo.jpg"},
@@ -14,18 +16,19 @@ public class dataPahlawan {
             {"Tan Malaka", "Tan Malaka atau Ibrahim gelar Datuk Sutan Malaka (lahir di Nagari Pandam Gadang, Suliki, Lima Puluh Kota, Sumatera Barat, 2 Juni 1897 – meninggal di Desa Selopanggung, Kediri, Jawa Timur, 21 Februari 1949 pada umur 51 tahun) adalah seorang pembela kemerdekaan Indonesia, tokoh Partai Komunis Indonesia, juga pendiri Partai Murba, dan merupakan salah satu Pahlawan Nasional Indonesia.", "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/TanMalaka_DariPendjara_ed3.jpg/330px-TanMalaka_DariPendjara_ed3.jpg"},
     };
 
-//    public static ArrayList<ModelPahlawan> ambilDataPahlawan() {
-//        ArrayList<ModelPahlawan> dataPahlawan = new ArrayList<>();
-//        for (String[] varData : data) {
-//            ModelPahlawan model = new ModelPahlawan();
-//            model.setNama(varData[0]);
-//            model.setTentang(varData[1]);
-//            model.setFoto(varData[2]);
-//
-//            dataPahlawan.add(model);
-//        }
-//
-//        return dataPahlawan;
+    public static ArrayList<ModelPahlawan> ambilDataPahlawan() {
+        ArrayList<ModelPahlawan> dataPahlawan = new ArrayList<>();
+        for (String[] varData : data) {
+            ModelPahlawan model = new ModelPahlawan();
+            model.setNama(varData[0]);
+            model.setTentang(varData[1]);
+            model.setFoto(varData[2]);
+
+            dataPahlawan.add(model);
+        }
+
+        return dataPahlawan;
     }
+}
 
 
